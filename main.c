1f1c2f529420b557ec0e58b6100ac6664bfa5c19
Mike <tankf33der@disroot.org>
2021-08-20 10:27:57 +0300

    .

--- main.c
+++ main.c
@@ -9,9 +9,10 @@
 int main(void) {
 	crypto_rng_ctx ctx;
 	uint8_t seed[32];
+	uint8_t out[32];
 
 	for(size_t i = 0; i < 32; i++) seed[i] = i;
 	crypto_rng_init(&ctx, seed);
-	
+	crypto_rng_read(&ctx, out, 32);
 	return 0;
 }
